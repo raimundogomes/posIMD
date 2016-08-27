0924c128b6b0634418dad8b3165a3b04ad218f47
raimundo gomes <raimundo gomes>
2016-08-26 21:29:59 -0300

    dd:x

--- movel/SGR/app/src/main/java/com/imd030/sgr/activities/PesquisarPacienteActivity.java
+++ movel/SGR/app/src/main/java/com/imd030/sgr/activities/PesquisarPacienteActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,7 +32,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PesquisarPacienteActivity extends Activity {
+public class PesquisarPacienteActivity extends Activity  implements
+        View.OnClickListener{
 
     private ProgressDialog load;
 
@@ -59,6 +61,10 @@ public class PesquisarPacienteActivity extends Activity {
         pacienteAdapter = new PacienteAdapter(getApplicationContext(),  listaPacientes);
 
         listView.setAdapter(pacienteAdapter);
+
+        Button pesquisar = (Button)findViewById(R.id.botao_pesquisar_paciente);
+
+        pesquisar.setOnClickListener(this);
 
 
     }
@@ -122,5 +128,64 @@ public class PesquisarPacienteActivity extends Activity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        String prontuario =  ((TextView)findViewById(R.id.text_prontuario)).getText().toString();
+        String cpf =  ((TextView)findViewById(R.id.text_cpf)).getText().toString();
+        String nome = ((TextView) findViewById(R.id.text_nome_paciente)).getText().toString();
 
+        if( !"".equals(prontuario) || !"".equals(cpf) || !"".equals(nome) ){
+
+            try {
+
+                JsonArrayRequest jsObjRequest = new JsonArrayRequest(Request.Method.GET,
+                        url, null, new Response
+                        .Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            Gson gson = new Gson();
+                            Log.i("Teste","teste 1");
+
+                            for (int i = 0; i < response.length(); ++i) {
+                                Paciente paciente = gson.fromJson(response.getJSONObject(i).toString(), Paciente.class);
+                                Log.i("Teste",paciente.getNome());
+                                listaPacientes.add(paciente);
+                            }
+                            pacienteAdapter = new PacienteAdapter(getApplicationContext(),  listaPacientes);
+
+                            listView.setAdapter(pacienteAdapter);
+                            pacienteAdapter.notifyDataSetChanged();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("Teste", error.toString());
+                        Toast.makeText(getApplicationContext(),
+                                "ERRO "+ error.toString(), Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+                jsObjRequest.setTag("REST");
+
+                queue.add(jsObjRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        else{
+            Toast toast = Toast.makeText(this, "Preencha o prontuário, CPF ou nome do paciente!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
 }
